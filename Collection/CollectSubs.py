e7d76eb1cecc2e88df8d058debfd1d7f3f912da3
GoodStuff11 <34872749+GoodStuff11@users.noreply.github.com>
2019-01-30 20:18:06 -0500

    Update and rename CollectSubs.py to Collection/CollectSubs.py

--- Collection/CollectSubs.py
+++ Collection/CollectSubs.py
@@ -12,7 +12,7 @@ for a in StackExchange.body.find_all('a', href=True):
     if s.startswith('https://') and s not in links and s.count('/') == 2 and s.endswith('.com') and s.count('?') == 0:
         links.append(a['href'])
 
-file = open('SE.txt','a+')
+file = open('../Data/SE.txt','a+')
 for i in links:
     file.write(i + '\n')
 file.close()
